6406a191a5ccdd15393bd69661bfb13271b8f177
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-21 17:02:08 +0100

    displays name1 is paired with name2

--- src/main/java/RandomOTronController.java
+++ src/main/java/RandomOTronController.java
@@ -26,10 +26,12 @@ public class RandomOTronController {
 
         get("/two", (req, res) -> {
            ArrayList<String> result = randomOTron.getTwoNames();
-
+           String randomName1 = result.get(0);
+           String randomName2 = result.get(1);
             HashMap<String, Object> model = new HashMap<>();
             model.put("randomOTron", randomOTron);
-            model.put("result", result);
+            model.put("randomName1", randomName1);
+            model.put("randomName2", randomName2);
             model.put("template", "two.vtl");
             return new ModelAndView(model, "layout.vtl");
         }, velocityTemplateEngine);
